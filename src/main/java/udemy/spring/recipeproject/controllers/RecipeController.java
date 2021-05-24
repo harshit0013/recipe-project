573b5ee8ed9679652bfe3a9f95a9de40051aa778
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-24 23:58:24 +0530

    Added Recipe add form page.

--- src/main/java/udemy/spring/recipeproject/controllers/RecipeController.java
+++ src/main/java/udemy/spring/recipeproject/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package udemy.spring.recipeproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.spring.recipeproject.commands.RecipeCommand;
 import udemy.spring.recipeproject.services.RecipeService;
 
 @Controller
@@ -19,5 +22,19 @@ public class RecipeController {
     {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model)
+    {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command)
+    {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
