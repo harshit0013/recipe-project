c20fc10d200996ceaee3b1e7f02c6c76a4a88180
harshit0013 <sharma.harshit2999@yahoo.com>
2021-06-02 19:08:30 +0530

    adding assignment for Number Format Exception.

--- src/test/java/udemy/spring/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/udemy/spring/recipeproject/controllers/RecipeControllerTest.java
@@ -60,6 +60,15 @@ class RecipeControllerTest {
     }
 
     @Test
+    void testGetRecipeNumberFormat() throws Exception {
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/adsf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     void testGetNewRecipeForm() throws Exception
     {
         RecipeCommand command = new RecipeCommand();
