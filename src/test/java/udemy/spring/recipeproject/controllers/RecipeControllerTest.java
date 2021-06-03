9983d3384bef5e314cd5be02db8bc4b7ada8dd63
harshit0013 <sharma.harshit2999@yahoo.com>
2021-06-03 21:15:25 +0530

    adding data validation

--- src/test/java/udemy/spring/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/udemy/spring/recipeproject/controllers/RecipeControllerTest.java
@@ -92,9 +92,28 @@ class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id","")
-                .param("description","Any description"))
+                .param("description","Any description")
+                .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    void testPostNewRecipeFormValidationFail() throws Exception
+    {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
+
     }
 
     @Test
